14c1e230444ff5fee9cc8b2038476cfe843463c0
aniket sharma <aniket965.as@gmail.com>
2017-03-09 23:02:11 +0530

    added knb

--- KNB.py
+++ KNB.py
@@ -4,3 +4,29 @@
 # euclidian distance for measuring distance
 #  directly proportional to dataset size
 
+import numpy as np;
+from sklearn import preprocessing,cross_validation,neighbors;
+import pandas as pd;
+df=pd.read_csv('breast-cancer-wisconsin.data');
+df.replace('?',-99999,inplace=True);
+print(df);
+#droping the useless coloumns
+df.drop(['id'],1,inplace=True);
+X= np.array(df.drop(['class'],1));
+y= np.array(df['class']);
+# splits the data for training and testing
+X_train,X_test,y_train,y_test = cross_validation.train_test_split(X,y,test_size=0.2);
+clf= neighbors.KNeighborsClassifier();
+clf.fit(X_train,y_train);
+accuracy = clf.score(X_test,y_test);
+print(accuracy);
+
+#prediction
+
+example= np.array([4,2,1,1,1,2,3,2,1]);
+# for reshaping the array 
+example = example.reshape(len(example),-1);
+prediction = clf.predict(example);
+print(prediction);
+# prediction 97.80%
+
