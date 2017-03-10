5df9967e3e3ba9e6355ed4404c17c1c93c72e752
aniket965 <aniket965.as@gmail.com>
2017-03-10 21:04:39 +0530

    fsKmeans have errors

---
+++ kmeans#2.py
@@ -0,0 +1,11 @@
+# import matplotlib.pyplot as plt
+# from matplotlib import style
+# style.use('ggplot');
+# import numpy as np;
+# from sklearn.cluster import KMeans
+# from sklearn import preprocessing,cross_validation
+# import pandas aPython s pd
+# df= pd.read_excel('titanic.xls');
+# df.drop(['body'],['name'],1,inplace=True);
+# df.convert_objects(bconvert_numeric=True);
+# df.fillna(0,inplace=True);
